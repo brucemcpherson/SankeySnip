723894e2ccf366b92ab7b42b7482ae4f3e2bf5b4
Bruce McPherson <bruce@mcpher.com>
2015-06-24 13:30:19 +0100

    updated by GasGit automation

--- scripts/Image.js
+++ scripts/Image.js
@@ -3,14 +3,14 @@ var Image = (function(image) {
   'use strict';
   
   // insert an image at row / column in the sheet
-  image.insert = function ( range,  png) {
-    range.getSheet().insertImage ( png ,  range.getColumn(), range.getRow());
+  image.insert = function ( range,  png , offx, offy) {
+    range.getSheet().insertImage ( png ,  range.getColumn(), range.getRow(),offx || 0 , offy || 0);
     
   };
   
   // place an image
   image.place = function (png) {
-    return image.insert ( SpreadsheetApp.getActiveRange() , png);
+    return image.insert ( SpreadsheetApp.getActiveRange() , png , 10,6);
   }
   
   return image;
