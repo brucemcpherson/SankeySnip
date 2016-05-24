44efd169f121b7db8a68bf503ac99567cac4543b
Bruce McPherson <bruce@mcpher.com>
2016-05-24 15:08:10 +0100

    updated by GasGit automation

--- scripts/App.js
+++ scripts/App.js
@@ -1,34 +1,38 @@
 /* Common app functionality */
 
 var App = (function startApp (app) {
-	'use strict';
-
-
-	// sets up all the app management divs.
-	app.initialize = function () {
-		var container = Utils.el("container");
-		var notification = Utils.elAdd(container,"div","notification");
-		notification.id = "notification";
-        Utils.elAdd(notification,"div","notification-close").id = "notification-close";
-		Utils.elAdd(notification,"div","notification-header").id = "notification-header";
-		Utils.elAdd(notification,"div","notification-message").id = "notification-message";
+  'use strict';
+  
+  
+  // sets up all the app management divs.
+  app.initialize = function () {
     
-		app.showNotification = function (header, text) {
-			Utils.el('notification-header').innerHTML=header;
-			Utils.el('notification-message').innerHTML =text;
-			Utils.show(notification);
-		};
-        
-        app.hideNotification = function () {
-          Utils.hide(notification);
-        };
-        
-        Utils.el('notification-close').addEventListener("click", function () {
-          Utils.hide(notification);
-        },false);
-
-	};
-
-	return app;
+    app.showNotification = function (header, text, toast) {
+      DomUtils.elem('notification-header').innerHTML=header;
+      DomUtils.elem('notification-message').innerHTML =text;
+      DomUtils.hide('notification-area',true, toast ? "notification-toast-header" : "notification-error-header"); 
+      DomUtils.hide('notification-header',true, toast ? "notification-toast-header" : "notification-error-header");  
+      DomUtils.hide('notification-message',true, toast ? "notification-toast-message" : "notification-error-message"); 
+      DomUtils.hide('notification-area',false);
+    };
+    
+    app.hideNotification = function () {
+      DomUtils.hide('notification-area',true);
+    };
+    
+    app.toast = function (header,text) {
+      app.showNotification (header,text,true);
+      setTimeout (function () {
+        app.hideNotification();
+      },Process.control.toast.interval);
+    }
+    
+    DomUtils.elem('notification-close').addEventListener("click", function () {
+      DomUtils.hide('notification-area',true);
+    },false);
+    
+  };
+  
+  return app;
 })(App || {});
 
