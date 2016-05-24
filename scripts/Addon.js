961bd12a1f156ad472a73468f7cee50693ea1cba
Bruce McPherson <bruce@mcpher.com>
2016-05-24 15:08:12 +0100

    updated by GasGit automation

--- scripts/Addon.js
+++ scripts/Addon.js
@@ -35,31 +35,5 @@ function showSankeySnip() {
   SpreadsheetApp.getUi().showSidebar(ui);
 }
 
-/**
-* Opens a picker dialog
-* will be kicked off as google.script.run from the sidebar
-*/
-function showPicker(content,pickerContent) {
-
-  var html = HtmlService.createTemplateFromFile('filepicker.html')
-    .evaluate()
-    .getContent();
-
-  var uiContent =  
-    HtmlService.createTemplate(html + "<script>\ndoIt(" +
-      JSON.stringify({
-        "content":content,
-        "pickerContent":pickerContent,
-        "token": ScriptApp.getOAuthToken(),
-        "key":'AIzaSyA2VcAc8N7EGAxgh4Tr1qJL_F27yuh4rx0'
-      }) +
-      ");\n</script>")
-      .evaluate()
-      .setWidth(Picker.settings.width)
-      .setHeight(Picker.settings.height)
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME);
-
-   SpreadsheetApp.getUi().showModalDialog(uiContent, "Save svg code");
-}
 
 
