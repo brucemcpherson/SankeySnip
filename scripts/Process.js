a3b8f4aac095f3ddaec59f64d6fefcf00d051d3f
Bruce McPherson <bruce@mcpher.com>
2016-06-24 18:10:41 +0100

    updated by GasGit automation

--- scripts/Process.js
+++ scripts/Process.js
@@ -11,7 +11,7 @@ var Process = (function (process) {
     purchaseLevel:'ssnipLevel',
     fullAccess:10,
     openAccess:true,
-    version:"2.1.0.1"
+    version:"2.2.0.0"
   };
   
   process.applyElementer  = function () {
