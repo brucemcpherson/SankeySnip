de91d3c689797a61efb012868331f9e6c4f1535a
Bruce McPherson <bruce@mcpher.com>
2016-10-14 12:11:01 +0100

    updated by GasGit automation

--- scripts/Server.js
+++ scripts/Server.js
@@ -1,9 +1,8 @@
 var Server = (function (ns) {
   
-  ns.generateTestData = function () {
+  ns.generateTestData = function (data) {
     var ss = SpreadsheetApp.getActiveSpreadsheet();
     var sh = ss.insertSheet();
-    var data = Process.control.sankey.testData;
     sh.getRange(1, 1, data.length, data[0].length).setValues(data);
     ss.setActiveSheet(sh);
     return data;
